0da82d1f35b0ba2ed5c5e8647403927b5680d209
fabi-roeckel <fabian.roeckel@glocally.de>
2023-05-01 16:42:49 +0200

    added function to check if scraping is allowed. And limit number of websites for testing purposes

--- src/scraper.py
+++ src/scraper.py
@@ -2,27 +2,105 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+import requests
+from urllib.parse import urlparse
+
+def is_scraping_allowed(url):
+    """
+    Checks if scraping is allowed for the given URL.
+
+    Args:
+        url (str): The URL to check.
+
+    Returns:
+        bool: True if scraping is allowed, False otherwise.
+    """
+    # Parse the URL to extract the domain.
+    domain = urlparse(url).netloc
+
+    # Get the URL for the robots.txt file for the domain.
+    robots_url = f"https://{domain}/robots.txt"
+
+    # Send a GET request to the robots.txt file.
+    try:
+        response = requests.get(robots_url)
+    except:
+        # If there was an error, assume scraping is not allowed.
+        return False
+
+    # Check if the status code of the response is OK.
+    if response.status_code != 200:
+        # If not, assume scraping is not allowed.
+        return False
+
+    # Parse the robots.txt file.
+    robots_txt = response.text
+
+    # Check if the user-agent "*" is disallowed from scraping any pages.
+    #if "User-agent: *\nDisallow: /" in robots_txt:
+    #    return False
+
+    # Check if the user-agent "*" is specifically disallowed from scraping the given URL.
+    if f"User-agent: *\nDisallow: {url}" in robots_txt:
+        return False
+
+    # If none of the above conditions are met, assume scraping is allowed.
+    return True
+
+
 def scrape_website(url):
-    # Send a GET request to the specified URL and get the HTML content
-    response = requests.get(url)
-    html_content = response.content
+    """
+    Scrapes the text of a website's landing page if allowed by its robots.txt file,
+    and removes unimportant HTML elements like the menu and footer.
+    """
+    try:
+        # Send a request to the website's robots.txt file to check if scraping is allowed
+        robots_url = url.rstrip('/') + '/robots.txt'
+        robots = requests.get(robots_url).text
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(html_content, 'html.parser')
+        # If scraping is allowed, send a request to the website's landing page
+        if 'User-agent: *\nDisallow:' in robots:
+            html = requests.get(url).text
+            soup = BeautifulSoup(html, 'html.parser')
 
-    # Find and remove the footer element(s) from the parsed HTML
-    for footer in soup.find_all('footer'):
-        footer.extract()
+            # Remove unimportant HTML elements like the menu and footer
+            for tag in soup(['nav', 'header', 'footer']):
+                tag.decompose()
 
-    # Extract all the text from the remaining HTML
-    text = soup.get_text()
+            # Extract the visible text from the HTML
+            text = soup.get_text(separator=' ')
 
-    # Remove any leading or trailing white spaces and return the text
-    return text.strip()
+            # Return the extracted text
+            return text
+        else:
+            # If scraping is not allowed, return a special message
+            return 'Scraping not allowed'
+    except:
+        # If an error occurs, return a special message
+        return 'Error scraping'
 
-# Create a dataframe with website URLs
-df = pd.read_csv("../data/raw/startup_list_seminar.csv")
 
-# Scrape each website URL and store the results in a new column
+# Read the CSV file into a pandas dataframe
+df = pd.read_csv('../data/raw/startup_list_seminar.csv')
 
-df['text'] = df['website_url'].apply(scrape_website)
+# Create a new dataframe to store the scraped data
+new_df = pd.DataFrame(columns=['website_url', 'text'])
+
+# Loop over each URL in the dataframe
+for i, url in enumerate(df['website_url']):
+    if i >= 10:
+        break
+    # Scrape the website and add the URL and text to the new dataframe
+    allowed = is_scraping_allowed(url)
+    print(allowed)
+    if allowed:
+        text = scrape_website(url)
+    else:
+        text = None
+
+    # print(text)
+    new_df = pd.concat([new_df, pd.DataFrame([[url, allowed, text]], columns=['url', 'allowed', 'text'])], ignore_index=True)
+
+# Save the new dataframe to a CSV file
+print("test")
+new_df.to_csv('startup_list_seminar-v0_1.csv', index=False)
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,5 +1,9 @@
 import unittest
 from src.demo import say_hello
+import unittest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from src.scraper import scrape_website
 
 
 class TestDemo(unittest.TestCase):
@@ -21,3 +25,56 @@ class TestDemo(unittest.TestCase):
 
         # define actions that are carried out after each test
         pass
+
+class TestScraper(unittest.TestCase):
+
+    def test_scrape_website_allowed(self):
+        # Test that the scrape_website function returns the expected text when scraping is allowed
+        url = 'http://books.toscrape.com/'
+        expected_text = 'Books to Scrape - Sandbox'
+
+        actual_text = scrape_website(url)
+        self.assertEqual(actual_text.strip(), expected_text)
+
+    def test_scrape_website_not_allowed(self):
+        # Test that the scrape_website function returns the expected text when scraping is not allowed
+        url = 'http://httpbin.org/deny'
+        expected_text = 'Scraping not allowed'
+
+        actual_text = scrape_website(url)
+        self.assertEqual(actual_text.strip(), expected_text)
+
+    def test_scrape_website_error(self):
+        # Test that the scrape_website function returns the expected text when an error occurs
+        url = 'http://thiswebsitedoesnotexist.com'
+        expected_text = 'Error scraping'
+
+        actual_text = scrape_website(url)
+        self.assertEqual(actual_text.strip(), expected_text)
+
+    def test_scrape_website_remove_html_elements(self):
+        # Test that the scrape_website function removes unimportant HTML elements from the scraped text
+        url = 'http://books.toscrape.com/'
+        expected_text = 'Books to Scrape - Sandbox'
+
+        actual_text = scrape_website(url)
+        self.assertNotIn('<nav', actual_text)
+        self.assertNotIn('<header', actual_text)
+        self.assertNotIn('<footer', actual_text)
+
+    def test_scrape_website_adds_to_dataframe(self):
+        # Test that the scrape_website function adds the scraped data to the new dataframe correctly
+        url = 'http://books.toscrape.com/'
+        expected_df = pd.DataFrame({'website_url': [url], 'text': ['Books to Scrape - Sandbox']})
+
+        df = pd.DataFrame({'website_url': [url]})
+        new_df = pd.DataFrame(columns=['website_url', 'text'])
+
+        for url in df['website_url']:
+            text = scrape_website(url)
+            new_df = pd.concat([new_df, pd.DataFrame([[url, text]], columns=new_df.columns)], ignore_index=True)
+
+        assert_frame_equal(new_df, expected_df)
+
+if __name__ == '__main__':
+    unittest.main()
